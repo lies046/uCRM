98fd7c4f848ac80d5e01916af0d4330d5d96d7b3
lies046 <skytree.skysky@gmail.com>
2022-09-19 11:34:16 +0900

    sec02_16

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup xmlns="http://www.w3.org/1999/html">
 import {Link } from "@inertiajs/inertia-vue3";
 </script>
 
@@ -7,4 +7,6 @@ import {Link } from "@inertiajs/inertia-vue3";
     <a href="/">あタグ経由です。</a><br>
     <Link href="/">Link経由です</Link><br>
     <Link :href="route('inertia.index')">名前付きルートの確認です</Link>
+    <br>
+    <Link :href="route('inertia.show', {id: 50})">ルートパラメーターのテストです</Link>
 </template>
