c72e615ac3f7709cf56a94187a742a0bc30add14
lies046 <skytree.skysky@gmail.com>
2022-11-06 11:08:26 +0900

    sec-031

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GuestLayout from '@/Layouts/Guest.vue'
+import Label from '@/Components/Label.vue'
+import Input from '@/Components/Input.vue'
+</script>
+
+<template>
+<GuestLayout>
+    <Label value="件名">タイトル</Label>
+    <Input modelValue="初期値が入ります"></Input>
+</GuestLayout>
+</template>
+
+
+
+<style scoped>
+
+</style>
