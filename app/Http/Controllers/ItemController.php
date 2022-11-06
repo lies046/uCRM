dead558c85fd3000e1ffad9ae6264991f7d2e045
lies046 <skytree.skysky@gmail.com>
2022-11-06 17:16:43 +0900

    sec-042

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -24,11 +24,11 @@ class ItemController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Inertia\Response
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
