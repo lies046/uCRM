3a55c70d90c80e50b0e696a3a734d1d154496f4c
lies046 <skytree.skysky@gmail.com>
2022-11-20 15:47:53 +0900

    sec-060

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,6 +6,7 @@ use App\Models\Customer;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use Inertia\Inertia;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
@@ -14,14 +15,12 @@ class CustomerController extends Controller
      *
      * @return \Inertia\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-//        $customers = Customer::searchCustomers()->select('id', 'name', 'kana','tel')->paginate(50);
-        $customers = Customer::searchCustomers('ノムラ');
+        $customers = Customer::searchCustomers($request->search)->select('id', 'name', 'kana','tel')->paginate(50);
 
-        dd($customers);
         return Inertia::render('Customers/Index', [
-            'customers' => Customer::select('id', 'name', 'kana','tel')->paginate(50)
+            'customers' => $customers
         ]);
     }
 
